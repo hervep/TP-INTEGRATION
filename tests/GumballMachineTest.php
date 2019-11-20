9a291b4c84d3e51abc5c739889e36645e6f4288d
Herve DJAKOU DJEDJAM <172897@S-L-IGLAB-11.umons.ac.be>
2019-11-20 09:51:06 +0100

    new3

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -6,10 +6,15 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
 {
     public $gumballMachineInstance;
     //prof
-    private $nom="x_test_tp"; // a changer
-    private $prenom="y_test_tp"; // a changer
-    private $date_naissance="0000-00-00"; // a changer
-    private $lieu_naissance="XY"; // a changer
+    private $nom="xx1"; // a changer
+    private $prenom="yy1"; // a changer
+    private $date_naissance="29-09-1980"; // a changer
+    private $lieu_naissance="ZZZ1"; // a changer
+
+     private $nom1="XXX2"; // a changer
+     private $prenom1="YYY2"; // a changer
+     private $date_naissance1="30-10-1981"; // a changer
+     private $lieu_naissance1="ZZZ2"; // a changer
     // cours
     private $intitule="***"; //a remplir
     private $duree="***";    //a remplir
@@ -28,28 +33,29 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
     {
         $max__id1=$this->gumballMachineInstance->GetLastIDP();
         $this->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom,$this->prenom,$this->date_naissance,$this->lieu_naissance));
+        $this->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom1,$this->prenom1,$this->date_naissance1,$this->lieu_naissance1));
         $max__id2=$this->gumballMachineInstance->GetLastIDP();
-        $this->assertEquals($max__id1+1,$max__id2);
+        $this->assertEquals($max__id1+2,$max__id2);
     }
     public function testAffichageProfAPI()
     {
-        /*à completer*/
+        /*ï¿½ completer*/
     }
      
     
     public function testAffichageCoursAVI()
     {
-        /*à completer*/
+        /*ï¿½ completer*/
     }
     public function testInsertC()
     {
        
-        /*à completer*/
+        /*ï¿½ completer*/
         
     }
     public function testAffichageCoursAPI()
     {
-        /*à completer*/
+        /*ï¿½ completer*/
     }
 
    
